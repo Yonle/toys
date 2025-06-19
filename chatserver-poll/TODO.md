fc31c53f67922f88c8d53f8f362277cb529c50c9
Yonle <yonle@lecturify.net>
2025-06-20 01:13:32 +0700

    replace the network stack
    - was confused why the hell fd is closed out of nowhere
    - checked strace, caught a new thread banishing these fd
    - likely culprit: net.Conn
    
    since i manage the fd myself, i don't think i need to use net.Conn
    so, i replace the entire crap with unix.Socket stack.
    
    and shit goes very well with no unexpected ghost close()
    
    now it's easier to do things this way. only refactor is needed
    
    Signed-off-by: Yonle <yonle@lecturify.net>

--- chatserver-poll/TODO.md
+++ chatserver-poll/TODO.md
@@ -1,5 +1,8 @@
 # TODO
 
-- ~~deal with half-read close on client side~~ shit still somewhat zonk
-- sometime if you send too fast / too long, POLLHUP sent out from nowhere
+- ~~deal with half-read close on client side~~
+  - ~~shit still somewhat zonk~~
+  - temporary removed
+- ~~sometime if you send too fast / too long, POLLHUP sent out from nowhere~~ done. net.Conn is the culprit.
 - probably separate somethings
+- refactor the chaos
--- chatserver-poll/main.go
+++ chatserver-poll/main.go
@@ -10,39 +10,63 @@ import (
 var BUF_SIZE = 4096
 var L_ADDR = "[::1]:1111"
 
+var lnfd int
 var fds []unix.PollFd
-var conns = map[int]net.Conn{}
-var blacklistWrite = map[int]struct{}{} // int -> fd
+var blacklistWrite = make(map[int]struct{}) // int -> fd
+
+// TODO: refactor this mess
 
 func main() {
-	ln, err := net.Listen("tcp", L_ADDR)
+	var err error
+	lnfd, err = unix.Socket(
+		unix.AF_INET6,
+		(unix.SOCK_STREAM | unix.SOCK_CLOEXEC | unix.SOCK_NONBLOCK),
+		unix.IPPROTO_TCP)
 	if err != nil {
 		panic(err)
 	}
 
-	defer ln.Close()
+	unix.SetsockoptInt(lnfd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+	unix.SetsockoptInt(lnfd, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
 
-	lnfile, err := GetListenerFile(ln)
-
+	var addr *net.TCPAddr
+	addr, err = net.ResolveTCPAddr("tcp6", L_ADDR)
 	if err != nil {
 		panic(err)
 	}
 
-	defer lnfile.Close()
-	lnfd := int(lnfile.Fd())
-	if err := unix.SetNonblock(lnfd, true); err != nil {
-		// bruh. we cannot NOT block it??
-		panic(err)
+	var iface *net.Interface
+	var zoneid uint32
+	iface, err = net.InterfaceByName(addr.Zone)
+	if err == nil {
+		zoneid = uint32(iface.Index)
+	}
+
+	sockAddr := &unix.SockaddrInet6{
+		Port:   addr.Port,
+		Addr:   [16]byte(addr.IP.To16()),
+		ZoneId: zoneid,
+	}
+
+	err = unix.Bind(lnfd, sockAddr)
+	if err != nil {
+		log.Fatalf("bind() failed: %v", err)
+	}
+
+	err = unix.Listen(lnfd, unix.SOMAXCONN)
+
+	if err != nil {
+		log.Fatalf("listen() failed: %v", err)
 	}
 
 	blacklistWrite[lnfd] = struct{}{} // NEVER LET broadcast() WRITE
 
 	log.Printf("Now polling for TCP server on %s (fd: %d)", L_ADDR, lnfd)
 
-	startPolling(ln, lnfd)
+	startPolling(lnfd)
 }
 
-func startPolling(ln net.Listener, lnfd int) {
+func startPolling(lnfd int) {
 	// put listener in
 	fds = append(fds, unix.PollFd{
 		Fd:     int32(lnfd),
@@ -51,12 +75,13 @@ func startPolling(ln net.Listener, lnfd int) {
 
 	for { // welcome to the jungle
 		n, err := unix.Poll(fds, -1)
+		if err == unix.EINTR {
+			// so skibidi, so gyat
+			// fuck it. we ball.
+			continue
+		}
+
 		if err != nil {
-			if err == unix.EINTR {
-				// so skibidi, so gyat
-				// fuck it. we ball.
-				continue
-			}
 			log.Fatal("poll() fail: ", err)
 		}
 
@@ -80,10 +105,12 @@ func startPolling(ln net.Listener, lnfd int) {
 				case lnfd: // FROM LISTENER?!?!?!???
 					panic("MAMAAAAAK THE KING HAS FALLEN!!!")
 				default:
-					fds[i].Events &^= (unix.POLLIN | unix.POLLRDHUP) // unsubscribe
+					//	fds[i].Events &^= (unix.POLLIN | unix.POLLRDHUP) // unsubscribe
+					makeItVanish(i)
+					i--
 				}
 
-				log.Printf("that kid with fd %d shut his ears off, but still wanna hear us yap. gotcha", ifd)
+				// log.Printf("that kid with fd %d shut his ears off, but still wanna hear us yap. gotcha", ifd)
 
 				continue
 			}
@@ -92,64 +119,56 @@ func startPolling(ln net.Listener, lnfd int) {
 				// from whom?
 				switch ifd {
 				case lnfd: // from listener? accept new guest
-					conn, err := ln.Accept()
-					if err != nil {
-						continue // nyeh.
-					}
-
-					connFile, err := GetConnFile(conn)
-					if err != nil {
-						conn.Close()
-						continue // nyeh.
-					}
-
-					connFd := int(connFile.Fd())
-
-					if err := unix.SetNonblock(connFd, true); err != nil {
-						// we cannot NOT block it???
-						conn.Close()
-						continue // nyeh.
-					}
-
-					// attach the conn to fds
-					fds = append(fds, unix.PollFd{
-						Fd:     int32(connFd),
-						Events: (unix.POLLIN | unix.POLLRDHUP),
-					})
-
-					conns[connFd] = conn
-					log.Printf("  new guest! look! it's fd is %d!", connFd)
+					handleNewGuest()
 				default: // beyond listener?
-					if _, ok := conns[ifd]; !ok { // if we don't know, just ignore
+					removeDisFd := handleGuest(ifd)
+					if removeDisFd {
+						makeItVanish(i)
+						i--
 						continue
-					}
-
-					log.Printf("  new event on %d!", ifd)
-
-					// make buffer...
-					buf := make([]byte, BUF_SIZE)
-
-					for {
-						n, err := unix.Read(ifd, buf)
-
-						if err == unix.EAGAIN {
-							log.Printf("    done reading data from %d :)", ifd)
-							break
-						}
-
-						if err != nil || n == 0 {
-							makeItVanish(i)
-							i--
-							break
-						}
-
-						d := buf[:n]
-
-						broadcast(ifd, d)
 					}
 				}
 			}
 		}
+	}
+}
+
+func handleNewGuest() {
+	// let's accept new guest!
+	nfd, _, err := unix.Accept(lnfd)
+	if err != nil {
+		log.Println("warn: i couldn't accept connection:", err)
+		return
+	}
+
+	if err := unix.SetNonblock(nfd, true); err != nil {
+		unix.Close(nfd)
+		log.Println("C_err: this fd couldn't set as noniblock:", err)
+		return
+	}
+
+	fds = append(fds, unix.PollFd{
+		Events: (unix.POLLIN | unix.POLLRDHUP),
+		Fd:     int32(nfd),
+	})
+
+	log.Printf("  look! new guest! it's fd %d!", nfd)
+}
+
+func handleGuest(nfd int) (removeDisFd bool) {
+	buf := make([]byte, BUF_SIZE)
+
+	for {
+		n, err := unix.Read(nfd, buf)
+		switch {
+		case err == unix.EAGAIN:
+			return // we're done reading
+		case err != nil:
+			removeDisFd = true
+			return
+		}
+
+		broadcast(nfd, buf[:n])
 	}
 }
 
@@ -162,33 +181,24 @@ func broadcast(bfd int, d []byte) {
 		}
 
 		for {
-			if _, err := unix.Write(fd, d); err != nil {
-				switch err {
-				case unix.EAGAIN:
-					continue
-				case unix.EPIPE:
-					unix.Shutdown(fd, unix.SHUT_WR)
-					blacklistWrite[fd] = struct{}{} // shh. don't talk
-					log.Printf("    %d closed read on their end", fd)
-					break
-				default:
-					// problem: the reader's forEach index is likely affected
-					makeItVanish(i)
-					i--
-					break
-				}
+			_, err := unix.Write(fd, d)
+
+			switch {
+			case err == unix.EAGAIN:
+				continue
+			case err == unix.EPIPE:
+				unix.Shutdown(fd, unix.SHUT_WR)
+				blacklistWrite[fd] = struct{}{} // shh. don't talk
+				log.Printf("    %d closed read on their end", fd)
+			case err != nil:
+				// problem: the reader's forEach index is likely affected
+				makeItVanish(i)
+				i--
 			}
 
 			break
 		}
 	}
-}
-
-func cleanupFd(fd int) {
-	conns[fd].Close() // this closes fd already.
-
-	delete(conns, fd)
-	delete(blacklistWrite, fd)
 }
 
 func makeItVanish(indx int) {
@@ -197,6 +207,8 @@ func makeItVanish(indx int) {
 	fds[indx] = fds[len(fds)-1] // move the latest crab here
 	fds = fds[:len(fds)-1]      // make the last thing vanish
 
-	cleanupFd(fd)
+	unix.Close(fd)
+	delete(blacklistWrite, fd)
+
 	log.Printf("  bye %d~", fd)
 }
